fdb06dc4ebcbc24b84a5883f7d8c13c3304d7f2e
tamenze <tamenze@gmail.com>
2025-12-11 11:52:29 -0500

    Split issues into dev and prod buckets

--- vite.config.ts
+++ vite.config.ts
@@ -5,38 +5,45 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr({
-    // Only transform when you opt in with ?react
-    include: "**/*.svg?react",
-    svgrOptions: {
-      // SVGO is on by default, but we pass config to keep viewBox
-      svgo: true,
-      svgoConfig: {
-        plugins: [
-          {
-            name: "preset-default",
-            params: { overrides: { removeViewBox: false } },
-          },
-        ],
+export default defineConfig(({ mode }) => {
+  const isProdBuild = mode === "production";
+
+  return {
+    plugins: [react(), tailwindcss(), svgr({
+      // Only transform when you opt in with ?react
+      include: "**/*.svg?react",
+      svgrOptions: {
+        // SVGO is on by default, but we pass config to keep viewBox
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: { overrides: { removeViewBox: false } },
+            },
+          ],
+        },
+      },
+    }),
+    //only upload source maps to sentry on prod builds
+    isProdBuild &&
+    sentryVitePlugin({
+      org: "na-h55",
+      project: "javascript-react-aword-prod",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        filesToDeleteAfterUpload: ["./dist/**/*.map"],
+      }
+    })
+  ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  }), sentryVitePlugin({
-    org: "na-h55",
-    project: "javascript-react-aword",
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    sourcemaps: {
-      filesToDeleteAfterUpload: ["./dist/**/*.map"],
+
+    build: {
+      sourcemap: true
     }
-  })],
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
-  build: {
-    sourcemap: true
   }
 });
